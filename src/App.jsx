492a0b48c67d945dd313ab3a06469d75079f7262
Rui Teixeira <rui.teixeira.t0123945@edu.atec.pt>
2023-10-16 23:05:24 +0100

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -7,50 +7,6 @@ import { PetsDetails } from './pages/PetsDetails'
 function App() {
   const [pets, setPets] = useState([])
 
-  // useEffect(() => {
-  //   //Permitir cancelar um pedido ao servidor
-  //   const abortController = new AbortController();
-
-  //   async function test() {
-  //     const data = {
-  //       name: "Bobby",
-  //       dateOfBirth: "2019-01-01",
-  //       breed: "Bulldog",
-  //     }
-
-  //     const petCreated = await createOrUpdatePet(data)
-
-  //     console.log(petCreated)
-
-  //     const allPets = await getAllPets()
-
-  //     console.log(allPets)
-
-  //     petCreated.name = "Fisher"
-  //     const petUpdated = await createOrUpdatePet(petCreated)
-
-  //     console.log(petUpdated)
-
-  //     const pet = await getPetById(petUpdated.id)
-
-  //     console.log(pet)
-
-  //     await deletePet(petUpdated.id)
-
-  //     const allPetsAfterDelete = await getAllPets()
-
-  //     console.log(allPetsAfterDelete)
-
-  //   }
-
-  //   test()
-
-  //   return () => {
-  //     //Cancelar o pedido caso o componente seja desmontado
-  //     abortController.abort();
-  //   };
-  // }, [])
-
   useEffect(() => {
     const abortController = new AbortController();
 
